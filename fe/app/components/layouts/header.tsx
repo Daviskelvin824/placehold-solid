0ddd3c6924486c6712d495b8ca93c89d3e68d8eb
reiyanyan <hariyantoriyan.hr@gmail.com>
2025-06-15 10:26:35 +0700

    fix: headerS

--- fe/app/components/layouts/header.tsx
+++ fe/app/components/layouts/header.tsx
@@ -6,11 +6,11 @@ export function Header({ className }: JSX.IntrinsicElements["div"]) {
   return (
     <div
       className={cn(
-        "absolute lg:fixed left-0 w-full py-6 px-4 sm:px-6 md:px-10 z-20 backdrop-blur-md",
+        "absolute lg:fixed left-0 w-full py-6 px-4 sm:px-6 md:px-10 z-20 backdrop-blur-md flex justify-center",
         className
       )}
     >
-      <div className="flex flex-nowrap items-center justify-between w-full gap-4 overflow-x-auto">
+      <div className="container flex flex-nowrap items-center justify-between w-full gap-4 overflow-x-auto">
         {/* <LogoStaticAnimated className="h-8 w-auto flex-shrink-0" /> */}
         <p className="text-3xl">Placehold.</p>
         <div className="flex-shrink-0">
